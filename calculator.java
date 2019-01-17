6fcecce3b6d9f030444c596839028807f2a278ca
Kishore <kishorekunal221@gmail.com>
2019-01-17 16:28:43 +0530

    kjjd

--- calculator.java
+++ calculator.java
@@ -9,13 +9,6 @@ class calculator.java
 			case 1:int sum=a+b;
 			       System.out.println(sum);
 			       break;
-			case 2:int sub=a-b;
-			       System.out.println(sub);
-			       break;
-			case 3:
-			       int mult=a*b;
-			       System.out.println(mult);
-			       break;
 			default:System.out.println("thats it");
 		}
 		//System.out.println("1.Add");
